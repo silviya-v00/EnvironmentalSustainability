41fced712bdd73c22018b9d608dc36369999be9c
silviya-v00 <sisi.vasileva2709@gmail.com>
2024-02-19 13:12:53 +0200

    Add initial project code

---
+++ EnvironmentalSustainabilityApp/EnvironmentalSustainabilityApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnvironmentalSustainabilityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
